3dfc1e62e4c7af5444309302007f1dc2ce3972d8
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-09-29 22:27:34 +0900

    Feat: router 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Myprofile from "./pages/Myprofile/Myprofile";
+import SignIn from "./pages/SignIn/SignIn";
 import SignUp from "./pages/SignUp/SignUp";
 import Userlist from "./pages/Userlist/Userlist";
 
@@ -8,8 +10,10 @@ const App = () => {
     <BrowserRouter>
       <div className="App">
         <Routes>
+          <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/userlist" element={<Userlist />} />
+          <Route path="/myprofile" element={<Myprofile />} />
         </Routes>
       </div>
     </BrowserRouter>
